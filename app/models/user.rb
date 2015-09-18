f505c8a6ce9cf9c4266e11d118fd4ef6dc96316b
louis <louis.chavane@gmail.com>
2015-09-18 15:26:05 +0200

    add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, presence: true
+
+  has_secure_password
+
 end
